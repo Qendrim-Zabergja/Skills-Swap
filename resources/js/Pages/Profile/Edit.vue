d8fbb971e6e6ffcad0e7199b8b854f2e21cfaec3
Qendrim-Zabergja <qendrim.zabergja02@gmail.com>
2025-05-11 23:16:17 +0200

    fixed timing in the chats

--- resources/js/Pages/Profile/Edit.vue
+++ resources/js/Pages/Profile/Edit.vue
@@ -599,10 +599,13 @@ const saveProfile = () => {
 
 const formatDate = (date) => {
   if (!date) return '';
-  return new Date(date).toLocaleDateString('en-US', {
+  return new Date(date).toLocaleString('en-US', {
     year: 'numeric',
     month: 'short',
     day: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit',
+    hour12: false
   });
 };
 
