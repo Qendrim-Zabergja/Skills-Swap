8efc2aec3dcbc5aef0cf5d11e8d5ec7014cd13ee
Qendrim-Zabergja <qendrim.zabergja02@gmail.com>
2025-06-12 18:54:39 +0200

    deploy

---
+++ build/assets/InputError-2LhnvTyj.js
@@ -0,0 +1,1 @@
+import{k as s,I as t,c as a,o as r,d as o,t as c}from"./app-RABvWGqE.js";const n={class:"text-sm text-red-600"},l={__name:"InputError",props:{message:{type:String}},setup(e){return(i,m)=>s((r(),a("div",null,[o("p",n,c(e.message),1)],512)),[[t,e.message]])}};export{l as _};
